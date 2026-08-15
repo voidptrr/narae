613d247af21d6fb546216caed03a0a064d4d4ede
voidptrr <bruno.tommaso@protonmail.com>
2026-08-15 02:10:51 +0100

    chore: lsp highlight only on c

--- lua/narae/core/lsp.lua
+++ lua/narae/core/lsp.lua
@@ -1,6 +1,11 @@
 local M = {}
 local language = require("narae.core.language")
 
+local function has_c_highlights(bufnr)
+  local extension = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":e"):lower()
+  return extension == "c" or extension == "h"
+end
+
 function M.setup()
   vim.diagnostic.config({
     signs = {
@@ -23,7 +28,7 @@ function M.setup()
       local client = vim.lsp.get_client_by_id(args.data.client_id)
 
       if client ~= nil then
-        client.server_capabilities.semanticTokensProvider = nil
+        vim.lsp.semantic_tokens.enable(has_c_highlights(args.buf), { bufnr = args.buf })
 
         if client:supports_method("textDocument/completion") then
           vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
